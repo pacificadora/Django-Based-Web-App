87464b9f205e784367c29eb137dcf36b884d57ca
pacificadora <44291397+pacificadora@users.noreply.github.com>
2020-08-24 19:18:34 +0530

    Add files via upload

---
+++ travello/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import destination
+# Register your models here.
+
+admin.site.register(destination)
+"""
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+  
+admin.site.register(Post, PostAdmin)
+"""
