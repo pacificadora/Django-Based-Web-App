d3c1654dae53af66cdc2b3b51e53c6aea4e7b3ea
pacificadora <44291397+pacificadora@users.noreply.github.com>
2020-08-24 19:09:27 +0530

    Update settings.py

--- telusko_django/settings.py
+++ telusko_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm^=nu@5(53vygpt9b059p$k+7zdcnzy33x@u+rg)m5+1&(6!+v'
+SECRET_KEY = 'blah-blah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
